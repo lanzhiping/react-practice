459b325834da91e1c32630be0229152bee4e9ecd
Zhiping Lan <Zhiping_Lan@epam.com>
2016-07-06 15:39:19 +0800

    add clean build folder plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
+let CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = [{
-    entry: ['server/index.js', 'client/index.js'],
+    entry: {
+        server: './server/index.js',
+        client: './client/index.js'
+    },
     output: {
         // Make sure to use [name] or [id] in output.filename
         // when using multiple entry points
         filename: '[name].bundle.js',
         chunkFilename: '[id].bundle.js',
-        path: './built'
-    }
+        path: './build'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['build'], {
+            root: __dirname, // An absolute path for the root.
+            verbose: true, // Write logs to console.
+            dry: false // Do not delete anything, good for testing.
+        })
+    ]
 }];
